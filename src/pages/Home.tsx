3975355f8cf92c6c16fbd1b613742d1eea53dd3e
DDDDanny <707956456@qq.com>
2024-10-30 15:05:42 +0800

    feat: 新增欢迎语提示动画

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../assets/styles/home.css'
+import BoxReveal from "@/components/ui/box-reveal.tsx";
 
 const HomePage: React.FC = () => {
 
   return (
     <>
       <div className='home-desc-container' >
-        <span className='hello-world-style'>Hello World！👋</span>
-        <span className='desc-style'>Exploring Every Moment of Life</span>
+        <BoxReveal boxColor={"#FAC145"} duration={0.5}>
+          <span className='hello-world-style'>Hello World！👋</span>
+        </BoxReveal>
+        <BoxReveal boxColor={"#FAC145"} duration={1}>
+          <span className='desc-style'>Exploring Every Moment of Life</span>
+        </BoxReveal>
       </div>
     </>
   );
