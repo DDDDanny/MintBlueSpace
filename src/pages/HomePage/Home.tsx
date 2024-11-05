6c272d9c7f5f8a36e5966090daba5b70cee6d83e
DDDDanny <707956456@qq.com>
2024-11-05 14:52:41 +0800

    feat: 文章块新增标签显示

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './home.css'
+import { Tag } from 'lucide-react'
 import BoxReveal from "@/components/ui/box-reveal.tsx";
 import IconCloud from "@/components/ui/icon-cloud.tsx";
 import SparklesText from "@/components/ui/sparkles-text.tsx";
@@ -70,7 +71,13 @@ const HomePage: React.FC = () => {
           <img className='list-item-image' src='/src/assets/images/001.png' alt='111'/>
           <div className='item-content-container'>
             <div className='item-title-desc-box'>
-              <span className='item-title'>这是MintBlue Space</span>
+              <div className='post-item-title-container'>
+                <span className='item-title'>这是MintBlue Space</span>
+                <div className='item-tag-container'>
+                  <Tag style={{ height: 12 }}/>
+                  精 选
+                </div>
+              </div>
               <span className='item-overview-content'>
                 从开始计划到设计最后到完成历时3年。哈哈哈，今天，它终于上线了
               </span>
