063931b47013e0c60bce1181146c9d3aac5ede42
DDDDanny <707956456@qq.com>
2024-11-06 19:19:19 +0800

    perf: 优化首页卡片

--- src/pages/GalleryPage/Gallery.tsx
+++ src/pages/GalleryPage/Gallery.tsx
@@ -1,31 +1,8 @@
 import './index.css'
-import {Tag} from "lucide-react";
 
 const GalleryPage = () => {
   return (
-    <div className='posts-page-layout'>
-      <div className='posts-tags-container'>
-        <span style={{ fontSize: 20,marginBottom: 5}}>Tags</span>
-        <div className='posts-tags-box'>
-          <div className='tag-item-box'>
-            <Tag style={{height: 15, marginRight: 3}}/>
-            <span>精选</span>
-          </div>
-          <div className='tag-item-box'>
-            <Tag style={{height: 15, marginRight: 3}}/>
-            <span>Python</span>
-          </div>
-          <div className='tag-item-box'>
-            <Tag style={{height: 15, marginRight: 3}}/>
-            <span>Vue</span>
-          </div>
-          <div className='tag-item-box'>
-            <Tag style={{height: 15, marginRight: 3}}/>
-            <span>React</span>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div>Gallery</div>
   );
 }
 
