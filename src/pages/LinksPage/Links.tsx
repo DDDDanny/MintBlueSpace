2f60ca9f5aa0e708c92c6ba71836aa42c45e6d5b
DDDDanny <707956456@qq.com>
2024-11-07 15:55:56 +0800

    feat：初始化Links Card

--- src/pages/LinksPage/Links.tsx
+++ src/pages/LinksPage/Links.tsx
@@ -1,5 +1,19 @@
+import './index.css'
+
 const LinksPage = () => {
-  return <div>Links</div>;
+  return (
+    <div className='links-main-layout'>
+      <div className='link-item-container'>
+        <div className='link-item-avatar'></div>
+        <div className='link-item-blog-info'>
+          <span className='link-item-name'>MintBlue</span>
+          <span className='link-item-desc'>
+            一个有趣的人一个有趣的人一个有趣一个有～
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LinksPage;
