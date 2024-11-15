d62206a4fe1d2b343f9224777f1e00cfe763853b
DDDDanny <707956456@qq.com>
2024-11-15 23:19:32 +0800

    perf: 打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
+import { compression } from 'vite-plugin-compression2'
 import react from '@vitejs/plugin-react'
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
