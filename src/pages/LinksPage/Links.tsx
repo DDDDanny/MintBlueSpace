0932b5b22a229bf9cf0b62786be8125d7f1bf746
DDDDanny <707956456@qq.com>
2024-11-30 21:50:18 +0800

    feat：完善友情链接内容

--- src/pages/LinksPage/Links.tsx
+++ src/pages/LinksPage/Links.tsx
@@ -1,5 +1,6 @@
 import './index.css'
-import { Link } from 'lucide-react'
+import { Link as LinkIcon } from 'lucide-react'
+import { Link } from "react-router-dom";
 import {useEffect, useState} from "react";
 import BlurFade from "@/components/ui/blur-fade.tsx";
 
@@ -23,21 +24,23 @@ const LinksPage = () => {
   return (
     <div className='links-main-layout'>
       <div className='links-desc-container'>
-        <Link style={{ minWidth: 17, color: '#FAC145' }}/>
+        <LinkIcon style={{ minWidth: 17, color: '#FAC145' }}/>
         <span style={{ marginLeft: 10 }}>
-          友情链接：这里汇集了大家的博客和个人网站～有兴趣交换友链的朋友可以点击这里进行申请～MBSpace期待新朋友的加入👏
+          友情链接：这里汇集了大家的个人网站和我常用的一些网站～MBSpace期待新朋友的加入👏
         </span>
       </div>
       <div className='links-items-container'>
         {links.map((linkItem, idx) => (
           <BlurFade delay={0.25 + idx * 0.05} inView key={idx}>
-            <div className='link-item-container'>
-              <img src='src/assets/001.jpeg' alt='Avatar' className='link-item-avatar'/>
-              <div className='link-item-blog-info'>
-                <span className='link-item-name'>{ linkItem.name }</span>
-                <span className='link-item-desc'>{ linkItem.desc }</span>
+            <Link to={linkItem.linkUrl} target="_blank" rel="noopener noreferrer">
+              <div className='link-item-container'>
+                <img src={linkItem.imageUrl} alt='Avatar' className='link-item-avatar'/>
+                <div className='link-item-blog-info'>
+                  <span className='link-item-name'>{linkItem.name}</span>
+                  <span className='link-item-desc'>{linkItem.desc}</span>
+                </div>
               </div>
-            </div>
+            </Link>
           </BlurFade>
         ))}
       </div>
